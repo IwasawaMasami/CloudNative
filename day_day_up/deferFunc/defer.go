1f738c578d033a90df9635658009016e25f5afaa
IwasawaMasami <592703320@qq.com>
2021-09-18 22:25:22 +0800

    Golang Basic

--- day_day_up/deferFunc/defer.go
+++ day_day_up/deferFunc/defer.go
@@ -5,7 +5,7 @@ import "fmt"
 func deferDemo() {
 
 	fmt.Println("start")
-	defer println("嘿嘿嘿") //defer把它后面的语句延迟执行
+	defer println("嘻嘻嘻嘻") //defer把它后面的语句延迟执行
 	defer println("hhh")
 	defer println("xxx") //多个defer按照先进后出的顺序执行
 	fmt.Println("end")
