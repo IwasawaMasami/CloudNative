9e442ebfdc33c48dfaecf67042873dacd48818dd
IwasawaMasami <592703320@qq.com>
2021-09-23 20:59:51 +0800

    update

--- day_day_up/calculation/calculation.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println(3 > 2)
-	
-
-}
--- day_day_up/printPath/path.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	path := "\"D:\\Go\\src\""
-	fmt.Println(path)
-}
